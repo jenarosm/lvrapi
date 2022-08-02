ec619caa7c061dc0e53262d465fdf5e04e4a497c
Jenaro <jenarosm@gmail.com>
2022-08-02 19:48:31 +1000

    Add project files.

---
+++ lvrapi/Controllers/LoanController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lvrapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController : ControllerBase
+    {
+
+        [HttpGet]
+        public Loan Get( double value, double ammount)
+        {
+            return new Loan { LoanAmmount = ammount, PropertyValue = value };
+        }
+
+        [HttpPost]
+        public double Post(Loan loan)
+        {
+            return loan.LVR;
+        }
+
+    }
+}
